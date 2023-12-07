a1b9e7563773c87fb014893d34d7a38f7729173d
NakkaAnilkumar <anilsunny1999@gmail.com>
2023-12-07 13:17:41 +0530

    Add project files.

---
+++ School.Test/UnitTest1.cs
@@ -0,0 +1,48 @@
+using Section_1._21;
+
+namespace School.Test
+{
+    [TestFixture]
+    public class SchoolDataTests
+    {
+        Student s = new Student();
+
+        Subject sub = new Subject();
+
+        Teacher ts = new Teacher();
+
+        [TestCase("Christopher", ExpectedResult = true)]
+        [TestCase("Sathiya", ExpectedResult = false)]
+        [TestCase("ganga", ExpectedResult = false)]
+        [TestCase("nandini", ExpectedResult = false)]
+        [TestCase("gnanasekar", ExpectedResult = false)]
+        public bool IsName(string search)
+        {
+
+            return s.Search(search);
+
+        }
+
+        [TestCase("Maths", ExpectedResult = true)]
+        [TestCase("Science", ExpectedResult = true)]
+        [TestCase("Biology", ExpectedResult = false)]
+        [TestCase("English", ExpectedResult = true)]
+        public bool IsSubject(string subj)
+        {
+
+            return sub.Search(subj);
+
+        }
+
+        [TestCase("Ram", ExpectedResult = true)]
+        [TestCase("Sakthi", ExpectedResult = true)]
+        [TestCase("Ganesh", ExpectedResult = true)]
+        [TestCase("malaiyan", ExpectedResult = false)]
+        public bool IsTeacher(string t)
+        {
+
+            return ts.Search(t);
+
+        }
+    }
+}
